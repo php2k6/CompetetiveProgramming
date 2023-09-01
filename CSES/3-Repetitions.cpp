14e2f07474b046817a8cae7e5c1eaee0f58815d8
PhP2k6 <142291570+PhP2k6@users.noreply.github.com>
2023-09-01 19:05:47 +0530

    Update 3-Repetitions.cpp

--- CSES/3-Repetitions.cpp
+++ CSES/3-Repetitions.cpp
@@ -3,19 +3,20 @@
 using namespace std;
 #define ll long long
 
-int main() {
+int main(){
   string a;
   cin >> a;
-  int c = 0, ans = 1;
-  char l = 'A';
-  for (char b: a) {
-    if (b==l) {
+  int c=0, ans=1;
+  char l='A';
+  for (char b : a){
+    if (b==l){
       c++;
-      ans = max(c, ans);
+      ans=max(c, ans);
 
-    } else {
-      l = b;
-      c = 1;
+    } 
+    else{
+      l=b;
+      c=1;
     }
   }
   cout << ans;
