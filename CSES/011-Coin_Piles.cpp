411fa1b4fee2e612ffda5a1bd38a21034b3e9608
PhP2k6 <142291570+PhP2k6@users.noreply.github.com>
2023-09-02 16:53:35 +0530

    Update 011-Coin_Piles.cpp

--- CSES/011-Coin_Piles.cpp
+++ CSES/011-Coin_Piles.cpp
@@ -8,14 +8,14 @@ void solve(){
     cin >> l >> r;
     if((l+r)%3==0 ){
         if(l==0 && r==0){
-            ans = "YES\n";
+            ans="YES\n";
         }
         else{
             if(l==0 || r==0 || (r*2)<l || (l*2)<r ){
-                ans = "NO\n";
+                ans="NO\n";
             }
             else{
-                ans = "YES\n";
+                ans="YES\n";
             } 
         } 
     }   
