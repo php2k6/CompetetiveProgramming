cca29bb5b4c05259ed6f9a11fbca0223dfa24c6f
PhP2k6 <142291570+PhP2k6@users.noreply.github.com>
2023-09-02 16:38:15 +0530

    Create 11-Coin_Piles.cpp

---
+++ CSES/11-Coin_Piles.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define ll long long
+void solve(){
+    ll l, r;
+    string ans;
+    cin >> l >> r;
+    if((l+r)%3==0 ){
+        if(l==0 && r==0){
+            ans = "YES\n";
+        }
+        else{
+            if(l==0 || r==0 || (r*2)<l || (l*2)<r ){
+                ans = "NO\n";
+            }
+            else{
+                ans = "YES\n";
+            } 
+        } 
+    }   
+    else {
+        cout << "NO\n";
+    }
+    cout << ans;
+}
+
+int main() {
+    ll t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+
+}
+
