7123b2836cd0ece64834721ff6defdec80d287e2
Prabhav Patel <142291570+php2k6@users.noreply.github.com>
2023-09-07 00:54:36 +0530

    Create 4A-Watermelon.cpp

---
+++ CodeForces/ProblemSet/4A-Watermelon.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#define ll long long
+using namespace std;
+
+
+int main() {
+    int w;
+    cin >> w;
+    if((w%2==0)&& w>2){
+        cout << "YES";
+    }else{
+        cout << "NO";
+    }
+    return 0;
+}
+
